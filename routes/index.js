968e92ee8f6e748e64f6b39023fbb2ef524f4411
Malek Bouneb <malek.bouneb3@gmail.com>
2022-12-08 13:24:18 +0100

    lol

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,17 @@ const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 
 router.post('/upload', async (req, res) => {
- const photoPath = `./tmp/${uniqid()}.jpg`;
- const resultMove = await req.files.photoFromFront.mv(photoPath);
- 
- if (!resultMove) {
-    const resultCloudinary = await cloudinary.uploader.upload('./tmp/photo.jpg');
-   res.json({ result: true, url: resultCloudinary.secure_url  });      
- } else {
-   res.json({ result: false, error: resultMove });
- }
- fs.unlinkSync('./tmp/photo.jpg');
-});
+  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const resultMove = await req.files.photoFromFront.mv(photoPath);
+  
+  if (!resultMove) {
+     const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+     fs.unlinkSync(photoPath)
+    res.json({ result: true, url: resultCloudinary.secure_url  });      
+  } else {
+    res.json({ result: false, error: resultMove });
+  }
+ });
 
 
 module.exports = router;
